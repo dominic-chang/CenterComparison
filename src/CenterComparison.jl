2921d5d030b764bf813886ccc35b1d8c8fd714e3
Dominic <dchang3419@hotmail.com>
2024-02-16 11:26:17 -0600

    Complete CenterComparison.jl

--- src/CenterComparison.jl
+++ src/CenterComparison.jl
@@ -3,10 +3,11 @@ import Comrade as CM
 using Plots
 using Pyehtim
 
-
-file = joinpath((@__DIR__), "..", "data","I1.fits")
+in_model = "I0_regrid"
+file = joinpath((@__DIR__), "..", "data", in_model*".fits")
 img = VIDA.load_image(file)
-img |> Plots.plot
+
+Plots.plot(img)
 
 bh = VIDA.Bhattacharyya(img)
 
@@ -36,24 +37,24 @@ VIDA.divergence(bh, m)
 
 #lower bounds on parameters
 lower = (
-    rad = μas2rad(8),
-    width = μas2rad(5),
+    rad = (8),
+    width = (5),
     α = -1,
     β = -1,
     ell = 0,
-    x = μas2rad(-10),
-    y = μas2rad(-10),
+    x = (-10),
+    y = (-10),
 )
 
 #upper bounds on parameters
 upper = (
-    rad = μas2rad(40),
-    width = μas2rad(20),
+    rad = (40),
+    width = (40),
     α = 1,
     β = 1,
     ell = 1,
-    x = μas2rad(10),
-    y = μas2rad(10),
+    x = (10),
+    y = (10),
 )
 # Defines a VIDAproblem type for optimization
 # Takes a divergenced function (bh) and a model function (ring_model)
@@ -64,7 +65,7 @@ f, t, (lb, ub) = VIDA.build_opt(prob, true)
 
 import OptimizationBBO as OBBO
 count = 0
-function callback(state, loss_val; doplot = false)
+function callback(state, loss_val; doplot = false) # callback function to print iterations and loss
     global count
     count += 1
     if count %100 |> iszero
@@ -73,7 +74,25 @@ function callback(state, loss_val; doplot = false)
     return false
 end
 xopt, opt_temp, divmin = vida(prob, OBBO.BBO_adaptive_de_rand_1_bin(); maxiters=50_000, callback=callback) #run problem 
+gr = VIDA.imagepixels(μas2rad(100), μas2rad(100), 100,100)
+VIDA.intensitymap(opt_temp, gr) |> Plots.plot
+xopt
+fig = triptic(img, opt_temp)
 
+# Save results
+outpath = joinpath((@__DIR__),"..","results")
+try
+    mkdir(outpath)
+catch e
+    println(e)
+end
+fileout = open(joinpath(outpath, in_model), "w")
+
+write(fileout, "upper = " * string(upper) * "\n")
+write(fileout, "lower = " * string(lower) * "\n")
+write(fileout, "best_fit = " * string(xopt))
+close(fileout)
+Plots.savefig(fig, joinpath(outpath, in_model*"_triptic.png"))
 
 
 
