68d6d47ecee70eacb417a25eb0808551ae2e7534
Dominic <dchang3419@hotmail.com>
2024-02-16 16:37:42 -0600

    Add DoublePowerLawModel

--- src/CenterComparison_mring.jl
+++ src/CenterComparison_mring.jl
@@ -2,12 +2,11 @@ using VIDA
 import Comrade as CM
 using Plots
 
-in_model = "I1_regrid"
+in_model = "I1_regrid"#"I1_a_0.94_i_45_sk_1.0_bphi_1.0_br_1.0_bvapp_0_gfact_3_mp_1.833_gp_-1.815_sp_3.574"
 file = joinpath((@__DIR__), "..", "data", in_model*".fits")
 img = VIDA.load_image(file)
 
 Plots.plot(img)
-
 
 m = CM.MRing(1.0,1.0) # Create ring of 1 radian radius
 Plots.plot(m) # plot the ring
@@ -20,22 +19,23 @@ m |> Plots.plot #shift the ring (adjust the position)
 
 
 # Create Our Ring Model
-# Params: rad, width, α, β, ell, x, y
+# Params: rad, width, α1, β1, α2, β2, ell, x, y
 
 function ring_model(params)
-    (;rad, width, α, β, ell, x, y) = params
-    m = CM.MRing(α, β) # Create ring of 1 radian radius
+    (;rad, width, α1, β1, α2, β2, ell, x, y) = params
+    m = CM.MRing([α1, α2], [β1, β2]) # Create ring of 1 radian radius
     m = CM.stretched(m, CM.μas2rad(rad), CM.μas2rad(rad*ell)) 
     m = CM.smoothed(m, CM.μas2rad(width)/(2*√(2*log(2)))) 
     return CM.ThreadedModel(CM.shifted(m, μas2rad(x), μas2rad(y)))
 end
-
 #lower bounds on parameters
 lower_bound = (
     rad = 8.0,
     width = 0.5,
-    α = -1.0,
-    β = -1.0,
+    α1 = -1.0,
+    β1 = -1.0,
+    α2 = -1.0,
+    β2 = -1.0,
     ell = 0.0,
     x = -10.0,
     y = -10.0,
@@ -43,16 +43,17 @@ lower_bound = (
 
 #upper bounds on parameters
 upper_bound = (
-    rad = 40.0,
-    width = 40.0,
-    α = 1.0,
-    β = 1.0,
+    rad = 50.0,
+    width = 80.0,
+    α1 = 1.0,
+    β1 = 1.0,
+    α2 = 1.0,
+    β2 = 1.0,
     ell = 1.0,
     x = 10.0,
     y = 10.0,
 )
-bh = VIDA.Bhattacharyya(img) # Loss function for comparison
-VIDA.divergence(bh, m)
+bh = VIDA.NxCorr(img) # Loss function for comparison
 
 # Defines a VIDAproblem type for optimization
 # Takes a divergenced function (bh) and a model function (ring_model)
@@ -71,7 +72,7 @@ function callback(state, loss_val; doplot = false) # callback function to print 
     end
     return false
 end
-xopt, opt_temp, divmin = vida(prob, OBBO.BBO_adaptive_de_rand_1_bin(); maxiters=50_000, callback=callback) #run problem 
+xopt, opt_temp, divmin = vida(prob, OBBO.BBO_adaptive_de_rand_1_bin(); maxiters=100_000, callback=callback) #run problem 
 gr = VIDA.imagepixels(μas2rad(100), μas2rad(100), 100,100)
 VIDA.intensitymap(opt_temp, gr) |> Plots.plot
 xopt
---
+++ src/DoublePowerLaw.jl
@@ -0,0 +1,18 @@
+struct DoublePowerLaw{T} <: CM.ComradeBase.AbstractModel
+    p1::T
+    p2::T
+end
+CM.ComradeBase.isprimitive(::DoublePowerLaw) = CM.ComradeBase.Isprimitive()
+function CM.ComradeBase.intensity_point(m::DoublePowerLaw, p)
+    (;p1, p2) = m
+    X, Y = p
+    r = hypot(X,Y)
+    return (r^(-p1))/(1+r^(-(p1+p2)))
+end
+
+Base.@constprop :aggressive @inline ComradeBase.visanalytic(::Type{<:DoublePowerLaw{T}}) where {T} = CM.ComradeBase.NotAnalytic()
+Base.@constprop :aggressive @inline ComradeBase.imanalytic(::Type{<:DoublePowerLaw{T}}) where {T} = CM.ComradeBase.IsAnalytic()
+Base.@constprop :aggressive @inline ComradeBase.ispolarized(::Type{<:DoublePowerLaw{T}}) where {T} = CM.ComradeBase.NotPolarized()
+function CM.ComradeBase.radialextent(m::DoublePowerLaw)
+    return 2
+end
