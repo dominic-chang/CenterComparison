c0fdd880610d70c5744c49c115cd40982bf4d09b
dominic-chang <dochang@g.harvard.edu>
2025-02-24 14:10:10 -0500

    Add script that generates fits file of thin sub ring

--- src/raytraceRings.jl
+++ src/raytraceRings.jl
@@ -1,8 +1,10 @@
+using VIDA
+
 include(joinpath((@__DIR__), "generate_n_ring.jl"))
 
 θo = 30 / 180 * π
 a = 0.94
-rs = 4.8
+rs = 4.0
 
 n = 1
 fig = Figure();
@@ -14,8 +16,8 @@ ymax = 10
 xlims!(ax, xmin, xmax)
 ylims!(ax, ymin, ymax)
 
-bulkres = 100
-screenres = 300
+bulkres = 400
+screenres = 200
 
 mat = generate_n_ring(rs, θo, a, n, bulkres, screenres, xmin, xmax, ymin, ymax;tolerance=1e-6, alpha=1e-5)
 αβvals = get_isoradial_curve(rs, θo, a, n, bulkres;tolerance=1e-6)
@@ -40,4 +42,10 @@ heatmap!(
 )
 scatter!(ax, αβvals)
 
+
 display(fig)
+
+grid = imagepixels(2xmax*μas2rad(5.03), 2ymax*μas2rad(5.03), screenres, screenres)
+intmap = IntensityMap(mat', grid)
+save_fits(joinpath(dirname((@__DIR__)), "thinRings", "m_d_5_03_rs_$(rs)_inc_$(Int(round(θo*180/pi)))_a_$(round(a;digits=2))_$(n)_ring.fits"), intmap)
+intmap  |> imageviz
